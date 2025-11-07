1a9d3b041c1290bf6282f103d916b89fb761454c
Sanskarsachan <Sanskar841singh@gmail.com>
2025-11-07 18:40:37 +0530

    feat(Header): Added header component

--- flix/app/(tabs)/index.tsx
+++ flix/app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { View } from "react-native";
 import Search from "../components/searchbar";
 import MovieList from "../components/movlist";
-
+import Header from "../components/header";
 export default function Index() {
   const [query, setQuery] = useState<string>("");
 
@@ -12,6 +12,7 @@ export default function Index() {
 
   return (
     <View className="flex-1">
+      <Header />
       <Search value={query} onChangeText={setQuery} onSubmit={handleSearch} />
       <MovieList query={query} />
     </View>
---
+++ flix/app/components/header.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+    return (
+        <View className='pt-4 items-center'>
+            <Image source={require('../../assets/images/Popflixlogo.png')} className='w-24 h-24 aspect-square align-middle' />
+        </View>
+    )
+}
